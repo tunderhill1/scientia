0c9de39d08d31fdd5828bcae6f24b33ae55d469c
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2022-06-12 17:46:49 +0100

    Docs: Add instructions to run Scientia over local network

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,6 +1,11 @@
 /**
  * NOTE: For Mac uses on macOS Monterey, the AirPlay Receiver service occupies port 5000. When running materials, please
  * disable it by opening System Preferences, navigating to Sharing and unchecking the AirPlay Receiver service.
+ *
+ * NOTE: To configure Scientia for testing on mobile devices over the local network, please add a new constant below
+ * that's mapped to your device's IP address and temporarily update the baseURL to use this instead of development:
+ * const local = 'http://X.X.X.X:5000'
+ * Next, start materials (and/or emarking) with the --host 0.0.0.0 argument.
  */
 
 const development = 'http://127.0.0.1:5000'
