bf24f8c124d451729f0d33580743337d2912fb8c
Killane, Oliver <oliver.killane20@imperial.ac.uk>
2022-10-05 16:25:00 +0000

    Fix: Add emarking to links dropdown.

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,6 +1,7 @@
 import {
   CameraReelsFill,
   EnvelopeFill,
+  ExplicitFill,
   Git,
   Icon,
   PeopleFill,
@@ -51,5 +52,11 @@ export const LINKS: Link[] = [
     description: 'Group project feedback',
     url: 'https://peer-assessment.doc.ic.ac.uk',
     icon: StarHalf,
+  },
+  {
+    title: 'eMarking',
+    description: 'Assessment feedback',
+    url: 'http://emarking.doc.ic.ac.uk/',
+    icon: ExplicitFill,
   },
 ]
