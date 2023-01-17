861caef1904c9f3c47913e7468732f410a2c3e2e
Ivan Procaccini <ivanprocaccini905@gmail.com>
2023-01-17 11:37:07 +0000

    Style: Make track item height 55px => 3.428rem (the height of a track item with estimated hours)

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,5 +1,5 @@
 export const NAVIGATION_HEIGHT = '4rem'
-export const TIMELINE_TRACK_HEIGHT = '3.25rem'
+export const TIMELINE_TRACK_HEIGHT = '3.438rem'
 export const DIVIDER_HEIGHT = '0.063rem'
 
 export const INDEXING_OFFSET = 1 /* Cols in grid 1-indexed */
